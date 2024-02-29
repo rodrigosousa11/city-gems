5aafa582a36ff3cca9f73db031cf85ce48a285cd
roh <rodrigosousa1105@gmail.com>
2024-02-29 14:38:19 +0000

    Cookie parser added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
@@ -8,6 +9,7 @@ const usersRouter = require("./routes/users");
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose
     .connect(process.env.MONGODB)
@@ -18,5 +20,5 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+	console.log("Server is running on  http://localhost:3000/");
 });
