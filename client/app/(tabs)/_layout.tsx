21491ae4be15521d8285c0d6eb01377be9bca0ed
roh <rodrigosousa1105@gmail.com>
2024-03-17 19:40:19 +0000

    Tab bottom bar icons added

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,9 +1,13 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import TabOneScreen from "./index";
-import TabTwoScreen from "./two";
+import Home from "./index";
+import Map from "./map";
+import Lists from "./lists";
 import Settings from "./settings";
 import { Button } from "react-native";
 import { useAuth } from "../context/AuthContext";
+import { Octicons } from "@expo/vector-icons";
+import { Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,27 +15,43 @@ export default function MyTabs() {
     const { onLogout } = useAuth();
 
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={{ 
+                tabBarActiveTintColor: 'black',
+                tabBarInactiveTintColor: 'gray'
+            }}
+        >
             <Tab.Screen
-                name="1"
-                component={TabOneScreen}
-                options={{ headerShown: false }}
+                name="Home"
+                component={Home}
+                options={{ 
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <Octicons name="home" size={24} color={color} /> }}
             />
             <Tab.Screen
-                name="2"
-                component={TabTwoScreen}
-                options={{ headerShown: false }}
+                name="Map"
+                component={Map}
+                options={{ 
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <Feather name="map" size={24} color={color} /> }}
+            />
+            <Tab.Screen
+                name="Lists"
+                component={Lists}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <Ionicons name="list" size={28} color={color} />
+                }}
             />
             <Tab.Screen
                 name="Settings"
                 component={Settings}
                 options={{
                     headerRight: () => (
-                        <Button
-                            onPress={onLogout}
-                            title="Logout"
-                        />
+                        <Button onPress={onLogout} title="Logout" />
                     ),
+                    tabBarIcon: ({ color }) => <Feather name="settings" size={24} color={color} />
+                    
                 }}
             />
         </Tab.Navigator>
