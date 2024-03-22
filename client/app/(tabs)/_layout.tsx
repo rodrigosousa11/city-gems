a782bd8eac09dca884d1d898ac81e327cd94210f
roh <rodrigosousa1105@gmail.com>
2024-03-22 18:33:23 +0000

    User location permission pop-up

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -3,44 +3,77 @@ import Home from "./index";
 import Map from "./map";
 import Lists from "./lists";
 import Settings from "./settings";
-import { Button } from "react-native";
+import { Button } from "react-native"; // Import PermissionsAndroid
 import { useAuth } from "../context/AuthContext";
 import { Octicons } from "@expo/vector-icons";
-import { Feather } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import * as Location from 'expo-location';
+import React, { useState, useEffect } from "react";
 
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
     const { onLogout } = useAuth();
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    let text = "Waiting..";
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
 
     return (
         <Tab.Navigator
-            screenOptions={{ 
-                tabBarActiveTintColor: 'black',
-                tabBarInactiveTintColor: 'gray'
+            screenOptions={{
+                tabBarActiveTintColor: "black",
+                tabBarInactiveTintColor: "gray",
             }}
         >
             <Tab.Screen
                 name="Home"
                 component={Home}
-                options={{ 
+                options={{
                     headerShown: false,
-                    tabBarIcon: ({ color }) => <Octicons name="home" size={24} color={color} /> }}
+                    tabBarIcon: ({ color }) => (
+                        <Octicons name="home" size={24} color={color} />
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Map"
                 component={Map}
-                options={{ 
+                options={{
                     headerShown: false,
-                    tabBarIcon: ({ color }) => <Feather name="map" size={24} color={color} /> }}
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="map" size={24} color={color} />
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Lists"
                 component={Lists}
                 options={{
                     headerShown: false,
-                    tabBarIcon: ({ color }) => <Ionicons name="list" size={28} color={color} />
+                    tabBarIcon: ({ color }) => (
+                        <Ionicons name="list" size={28} color={color} />
+                    ),
                 }}
             />
             <Tab.Screen
@@ -50,8 +83,9 @@ export default function MyTabs() {
                     headerRight: () => (
                         <Button onPress={onLogout} title="Logout" />
                     ),
-                    tabBarIcon: ({ color }) => <Feather name="settings" size={24} color={color} />
-                    
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="settings" size={24} color={color} />
+                    ),
                 }}
             />
         </Tab.Navigator>
