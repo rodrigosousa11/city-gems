50fb735198227590965055f1be3192d0eede3e89
roh <rodrigosousa1105@gmail.com>
2024-03-22 18:34:47 +0000

    Map view adjustments

--- client/app/(tabs)/map.tsx
+++ client/app/(tabs)/map.tsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
 
-const INITIAL_REGION = {
-    latitude: 38.736946,
-    longitude: -9.142685,
+const DEFAULT_REGION = {
+    // Porto, Portugal
+    latitude: 41.1496,
+    longitude: -8.6109,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
 };
 
-
-
 export default function Map() {
+
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} 
-            provider={PROVIDER_GOOGLE}
-            initialRegion={INITIAL_REGION}
+            <MapView
+                style={styles.map}
+                provider={PROVIDER_GOOGLE}
+                initialRegion={DEFAULT_REGION}
+                showsUserLocation
+                showsMyLocationButton
             />
         </View>
     );
-
 }
 
 const styles = StyleSheet.create({
